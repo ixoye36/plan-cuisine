ffe66cbda65483b1b98abbd2ac9254b2e0c10ee5
Peniel Mateta <penielmateta@gmail.com>
2022-08-10 18:09:40 +0200

    feat(googleads): adding ga to website

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,24 @@ import '../styles/globals.css';
 import "../styles/Styles.css";
 import "../styles/Button.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
   return getLayout(
     <>
+      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+      <Script strategy="lazyOnload" id="gtag">
+        {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
